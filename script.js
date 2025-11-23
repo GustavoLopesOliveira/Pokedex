395104e1544868fa34301a75e58eed2d94b5fb16
Gustavo Lopes <gustavolopesoliveira285@gmail.com>
2025-11-23 18:09:59 -0300

    upg : js

--- script.js
+++ script.js
@@ -1,6 +1,50 @@
 const url = "https://pokeapi.co/api/v2/pokemon/"
 
-const id_pokemon = 1
+const buttonLeft = document.getElementById('buttonLeft')
+const buttonRight = document.getElementById('buttonRight')
+const containerCards = document.getElementById('containerCards')
+const min = 1
+
+
+
+window.onload( () => { 
+    loadPage()
+})
+
+buttonLeft.addEventListener('click',() => {
+    min = min - 20
+    if(min <= 0) min = 1
+    loadPage()
+})
+
+buttonRight.addEventListener('click', () => {
+    min = min + 20
+    loadPage()
+})
+
+async function loadPage(){
+    containerCards.innerHTML = ''
+
+    for(let i = min; i <= min + 20; i++){
+        let data = await fetchData(i)
+        createCard(data)
+    }
+}
+
+async function fetchData(id){
+    const response = await fetch(url+id)
+    const data = await response.json()
+    return data
+}
+
+function createCard(data){
+    const newDiv = `
+        <div class = 'card'>
+            
+        </div>
+    ` 
+    containerCards.append(newDiv)
+}
 
 
 
